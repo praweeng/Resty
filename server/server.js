6fe00ceb40487be19e3f9d0f698d36b9971bb0ca
praveen <praveengonthina@gmail.com>
2017-07-22 00:10:11 +0530

    Implemented Apollo client

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://praween:Graham_50@ds115583.mlab.com:15583/resty-db';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
